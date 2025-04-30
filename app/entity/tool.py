fdc6d56df096787ba8920fd0f557c55bcbf3c54f
koljapluemer <github@koljapluemer.com>
2025-04-30 10:10:24 +0200

    add Tool model, make migrations

--- app/entity/tool.py
+++ app/entity/tool.py
@@ -1,1 +1,13 @@
 # *a language app, software, tool, resource, etc.*
+
+from django.db import models
+
+class Tool(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    link = models.URLField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- app/models.py
+++ app/models.py
@@ -1,2 +1,10 @@
 # should NOT contain actual models, since we're using entities/
 # only pulls in models so django can use them
+
+
+from .entity.tool import Tool
+
+# This is required for Django to recognize the models
+__all__ = [
+    'Tool',
+]
